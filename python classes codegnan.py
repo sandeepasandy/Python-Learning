042ab7794819912a9d0f1ebe3790b107e42bbc81
sandeepasandy <163431648+sandeepasandy@users.noreply.github.com>
2026-07-16 12:49:17 +0530

    Add files via upload
    This Python file covers fundamental programming concepts, including tokens, keywords, identifiers, operators, and simple example programs for beginners.
    DAY1  OF LEARNING #PYTHON LEARNING IN 100 DAYS

---
+++ python classes codegnan.py
@@ -0,0 +1,75 @@
+'''---Tokens:
+-->Tokens are smallest individual parts or units in the program
+--key words:
+----keywords are the reserved words
+
+if
+while
+else
+return
+for
+and
+in
+identifiers:
+---variable
+function name
+class name
+operators:
++ - =
+4.literal:
+int,str,tuple,list
+
+Operators:
+   1 arithmetic operator:+,-,*,%,**
+   2 Assignment operator
+    =,+=,-=,*=,%=,/=
+   3 comparision
+    ==,!=,<=,>=,>,<
+    4 logical
+    and or not
+    5 identity
+    is isnot
+    is look for object
+    == checks the values are same or not
+    6 membership
+    in not in
+    7 bitwise
+    &,|,^,<<,>>
+
+
+num=9
+num_2=7
+print(num + num_2)
+print(num - num_2)
+print(num * num_2)
+print(num ** num_2)
+print(num % num_2)
+print(num / num_2)
+
+num=8
+num_2=6
+print(num ==num_2)
+print(num != num_2)
+print(num > num_2)
+print(num < num_2)
+
+num=78
+num_2=89
+print(num<num_2 and num_2<100)
+print(num>num_2 or num_2<100)
+print(not(num<num_2 and num_2<100))
+
+num=9
+num_2=9
+print(num is num_2)
+print(num == num_2)
+
+so=[1,2,2,4,5,]
+print(7 in so)
+print(7 not in so)
+'''
+print(5 & 3)
+print(5 | 3)
+print(5 << 3)
+print(5 >> 3)
+
